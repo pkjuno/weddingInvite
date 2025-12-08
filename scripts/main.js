8c991a5f752e0b851d264f731939b14291281c09
DESKTOP-QIBNG2D\pkjun <pkjuno@nate.com>
2025-12-08 20:45:02 +0900

    원복

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-window.location.href = "https://pkjuno.github.io/weddingInvite";
-
 //var images = ['images/A06I6424.JPG', 'images/A06I5339.JPG', 'images/A06I5706.JPG', 'images/A06I5801.JPG', 'images/A06I5871.JPG', 'images/A06I6052.JPG', 'images/A06I6250.JPG', 'images/outSide1.jpg','images/outSide2.jpg', 'images/outSide3.jpg'];
 var images = ['images/readyForGallery.png'];
 var currentIdx = 0;
