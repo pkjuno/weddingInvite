12b8b6c5ab7a560a068dc131947bf318c3bd0f5d
DESKTOP-QIBNG2D\pkjun <pkjuno@nate.com>
2026-02-02 19:40:56 +0900

    dd

--- scripts/main.js
+++ scripts/main.js
@@ -196,7 +196,7 @@ function shareKakao() {
             content: {
                 title: '박준호 ♥ 김보미 결혼합니다',
                 description: '2026년 3월 15일 일요일 오후 1시 40분\n상암 월드컵 컨벤션 임페리얼 홀',
-                imageUrl: 'https://pkjuno.github.io/weddinginvite/images/img_34.jpg',
+                imageUrl: 'https://pkjuno.github.io/weddinginvite/images/img_33.jpg',
                 link: { mobileWebUrl: 'https://pkjuno.github.io/weddingInvite', webUrl: 'https://pkjuno.github.io/weddingInvite' }
             },
             buttons: [
